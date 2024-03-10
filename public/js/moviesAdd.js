c70ec61d2e810568a3b78e1e76699007b454b0bc
GabrielDko <gabriielrobless@gmail.com>
2024-03-10 00:54:43 -0300

    microdesafio 2

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -15,4 +15,29 @@ window.onload = function(){
 
 
 
+    let estadoSecreto = 0;
+    const movieTitle = document.getElementById('titulo');
+    
+    movieTitle.addEventListener('keydown', (e) => {
+        if (estadoSecreto === 0 && e.key === 's') {
+            estadoSecreto = 1;
+        } else if (estadoSecreto === 1 && e.key === 'e') {
+            estadoSecreto = 2;
+        } else if (estadoSecreto === 2 && e.key === 'c') {
+            estadoSecreto = 3;
+        } else if (estadoSecreto === 3 && e.key === 'r') {
+            estadoSecreto = 4;
+        } else if (estadoSecreto === 4 && e.key === 'e') {
+            estadoSecreto = 5;
+        } else if (estadoSecreto === 5 && e.key === 't') {
+            estadoSecreto = 6;
+        } else {
+            estadoSecreto = 0;
+        }
+    
+        if (estadoSecreto === 6) {
+            alert('SECRETO MÁGICO');
+            estadoSecreto = 0;
+        }
+    });
 }
